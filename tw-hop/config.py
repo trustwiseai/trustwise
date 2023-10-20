03eba3101cc6e3d114c4dd5e6ea1c78b08229fc6
Nihal Bhatnagar <126560262+Nihal0130@users.noreply.github.com>
2023-10-20 16:16:23 +0100

    Commit for files to create a pip package for TW

---
+++ tw-hop/config.py
@@ -0,0 +1,3 @@
+base_validate_url = 'http://54.144.17.111:8000/validate_tw_key'
+
+evaluate_url = 'http://54.144.17.111:8080/evaluate'
---
+++ tw-hop/functions.py
@@ -0,0 +1,77 @@
+import requests
+import json
+import uuid
+import config
+import pandas as pd
+
+
+def validate_api_key(api_key):
+    base_validate_url = config.base_validate_url
+    headers = {'accept': 'application/json'}
+    params = {'api_key': api_key}
+    response = requests.post(base_validate_url, headers=headers, params=params)
+    user_id = response.json()
+    return user_id
+
+
+class Observability:
+
+    # TODO add a docstring for this function
+    def set_api_key(self, api_key: str):
+        if api_key is not None:
+            self._user_id = validate_api_key(api_key)
+            print("User Authenticated!")
+        else:
+            raise ValueError("API Key is invalid!")
+
+    # TODO add a docstring for this function
+    def evaluate(self, query, response):
+
+        user_id = self._user_id
+
+        data = {
+            "response": response.response,
+            "source_nodes": []
+        }
+
+        data['user_id'] = user_id
+
+        cons = ''
+        for node in response.source_nodes:
+            cons = cons + node.text
+
+        for node in response.source_nodes:
+            data["source_nodes"].append({
+                "node_id": node.id_,
+                "text": node.text,
+                "score": node.score
+            })
+
+        random_uuid = uuid.uuid4()
+        scan_id = str(random_uuid)
+
+        questions = [query]
+        answers = [response.response]
+        contexts = [cons]
+
+        data_dict = {
+            "query": questions,
+            "response": answers,
+            "context": contexts
+        }
+
+        data['scan_data'] = {
+            "scan_id": scan_id,
+            "scan_data": data_dict}
+
+        url = config.evaluate_url
+
+        json_data = json.dumps(data)
+        headers = {'accept': 'application/json'}
+        res = requests.post(url, headers=headers, data=json_data, json=None)
+
+        dat = res.json()
+        df = pd.DataFrame(dat)
+        print("")
+        print("Results:")
+        return df
