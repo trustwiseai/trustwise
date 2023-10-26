3d40f83ded92ce92c007238e26dcfa97ade00f77
Nihal Bhatnagar <nihal@trustwise.ai>
2023-10-26 18:25:11 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="trustwise",
-    version="1.0.4",
+    version="1.0.0",
     description="Hallucinations Observability Plugin",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
