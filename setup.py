39302e75c89f83e4bf478b13ec5110fc46e82eed
Jaisantosh <jai@trustwise.ai>
2024-03-14 09:23:03 -0400

    change version to 2.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="trustwise",
-    version="2.2.1",
+    version="2.2.2",
     description="Trustwise PyPi Package",
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type="text/markdown",
